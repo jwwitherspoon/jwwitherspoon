9d4b6b2632b37c2704524d0ee7521c1b13810f4b
jwwitherspoon <jwwitherspoon@gmail.com>
2023-09-27 22:45:22 -0400

    Add base and homepage styles Add btn class for styling buttons

--- src/Home.svelte
+++ src/Home.svelte
@@ -2,25 +2,35 @@
     import {link} from 'svelte-spa-router';
 </script>
 
-<h1 class="text-center">Projects</h1>
-<div>
-    <a href="/musical-typing" use:link class="btn btn-primary">Musical Typing</a>
-</div>
-<div>
-    <a href="/beef-generator" use:link class="btn btn-primary">Beef Generator</a>
-</div>
-<div>
-    <a href="/jazz-practice" use:link class="btn btn-primary">Jazz Practice Tools</a>
-</div>
-<div>
-    <a href="/sudoku-solver" use:link class="btn btn-primary">Sudoku Solver</a>
-</div>
-<div>
-    <a href="/ultimate-tic-tac-toe" use:link class="btn btn-primary">Ultimate Tic-Tac-Toe</a>
+<div id="grid-container">
+    <a class="btn" href="/musical-typing" use:link>Musical Typing</a>
+    <a class="btn" href="/beef-generator" use:link>Beef Generator</a>
+    <a class="btn" href="/jazz-practice" use:link>Jazz Practice Tools</a>
+    <a class="btn" href="/sudoku-solver" use:link>Sudoku Solver</a>
+    <a class="btn" href="/ultimate-tic-tac-toe" use:link>Ultimate Tic-Tac-Toe</a>
 </div>
 
 <style scoped>
-    .btn {
-        margin: 10px;
+    #grid-container {
+        display: grid;
+        padding: 20px;
+        grid-template-columns: 1fr 1fr;
+        gap: 30px;
+    }
+
+    @media only screen and (max-width: 600px) {
+        #grid-container {
+            grid-template-columns: 1fr;
+        }
+    }
+
+    a {
+        font-size: 2rem;
+    }
+
+    @media only screen and (max-width: 600px) {
+        a {
+            font-size: 1.5rem;
+        }
     }
 </style>
