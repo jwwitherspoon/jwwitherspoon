056c3c0768abaaae33d12689243d7f7f0858c612
jwwitherspoon <jwwitherspoon@gmail.com>
2023-10-04 19:38:41 -0400

    Reset background color

--- src/SudokuSolver.svelte
+++ src/SudokuSolver.svelte
@@ -208,6 +208,7 @@
         for (let i=0; i<9; i++) {
             for (let j=0; j<9; j++) {
                 document.getElementById(i + "-" + j).value = "";
+                document.getElementById(i + "-" + j).style.backgroundColor = "initial";
                 document.getElementById(i + "-" + j).style.color = "black";
             }
         }
